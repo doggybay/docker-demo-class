aeaa369b0e7c9977ce092962ac9fd1703108ec8f
doggybay <julio.miranda12@gmail.com>
2020-02-14 23:42:09 -0700

    updated connection

--- docker-demo-server/knexfile.js
+++ docker-demo-server/knexfile.js
@@ -1,9 +1,9 @@
-let connectionString = process.platform === 'win32' ? 'postgres://postgres:root@localhost/todos' : 'postgres://localhost/todos'
+let connectionString = process.platform === 'win32' ? 'postgres://postgres:pass@localhost/todos' : 'postgres://localhost/todos'
   
   module.exports = {
     development: {
         client: 'pg',
-      connection: process.env.DATABASE_URL,
+      connection: connectionString,
         migrations: {
             directory: __dirname + '/db/migrations',
           },
